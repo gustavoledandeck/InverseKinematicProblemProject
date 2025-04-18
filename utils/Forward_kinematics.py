9b3b9052ae2b2363d70016686464f2dd65ffe7a5
Gustavo Ledandeck <guledandeck2@hotmail.com>
2025-04-18 20:47:45 -0300

    FK sketch file with 3 and 4-DOF model, and a visual plot to see it.

---
+++ utils/Forward_kinematics.py
@@ -0,0 +1,48 @@
+import nummpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+
+class ForwardKinematics:
+    """
+    Forward Kinematics implementation for robotic arms with different DOF configurations.
+    """
+
+    def __init__(self):
+        """
+        Initalize the FK model with link lengths
+
+        TO-DO
+        """
+
+
+    def forward_kinematics_4dof(self):
+        """
+        Calculate forward kinematics for a 4-DOF robotic arm R³ space
+        TO-DO
+        :return:
+        """
+
+    def forward_kinematics_3dof(self):
+        """
+        Calculate forward kinematics for a 3-DOF robotic arm R² space
+        In 3-DOF we have a Planar robot with 3 revolute joints to control.
+        TO-DO
+        :return:
+        """
+
+    def visualize_arm_4dof(self):
+        """
+        Visualize the 4-DOF robotic arm configuration (R³ space)
+        :return:
+        """
+
+    def visualize_arm_3dof(self):
+        """
+        Visualize the 4-DOF robotic arm configuration (R³ space)
+        :return:
+        """
+
+if __name__ == '__main__':
+    """
+        TO-DO
+    """
